b672de5394bf287e6fcdfc3c5758a7a0f306dc99
Segev Cohen <segevcohen98@gmail.com>
2025-02-03 22:46:22 +0200

    Search functionality

--- app/src/main/java/com/example/ex01_recycleview/activitys/CustomeAdapter.java
+++ app/src/main/java/com/example/ex01_recycleview/activitys/CustomeAdapter.java
@@ -1,8 +1,12 @@
 package com.example.ex01_recycleview.activitys;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,23 +17,27 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.ex01_recycleview.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class CustomeAdapter extends RecyclerView.Adapter<CustomeAdapter.MyViewHolder>{
+public class CustomeAdapter extends RecyclerView.Adapter<CustomeAdapter.MyViewHolder> implements Filterable {
 
     private ArrayList<DataModel> dataSet;
+    private ArrayList<DataModel> filteredList;
+    private Context context;
 
-    public CustomeAdapter(ArrayList<DataModel> dataSet) {
+    public CustomeAdapter(ArrayList<DataModel> dataSet, Context context) {
         this.dataSet = dataSet;
+        this.filteredList = new ArrayList<>(dataSet);
+        this.context = context;
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
-
         CardView cardView;
         TextView textViewName;
         TextView textViewDescription;
         ImageView imageView;
 
-        public MyViewHolder(@NonNull View itemView) { // הוצאת כל האיברים הקיימים בתוך ה-cardview ושומר את האיברים בתוך המערכת
+        public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             cardView = itemView.findViewById(R.id.cardViewShow);
             textViewName = itemView.findViewById(R.id.textViewNameOfCharacter);
@@ -41,24 +49,63 @@ public class CustomeAdapter extends RecyclerView.Adapter<CustomeAdapter.MyViewHo
     @NonNull
     @Override
     public CustomeAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        // פורס את הרכיב על השורה שלנו
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview, parent, false);
-
-        MyViewHolder myViewHolder = new MyViewHolder(view);
-
-        return myViewHolder;
+        return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CustomeAdapter.MyViewHolder holder, int position) {
-        //  לולאה מאחורי הקלעים בונים שורה אחת בתוך הRV מבצעת את זה אוטומטית
-        holder.textViewName.setText(dataSet.get(position).getName());
-        holder.textViewDescription.setText(dataSet.get(position).getDesc()); //חייב להוסיף תיאור
-        holder.imageView.setImageResource(dataSet.get(position).getImage());
+        DataModel item = filteredList.get(position);
+
+        holder.textViewName.setText(item.getName());
+        holder.textViewDescription.setText(item.getDesc());
+        holder.imageView.setImageResource(item.getImage());
+
+        holder.cardView.setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle(item.getName());
+            builder.setMessage(item.getDesc());
+            builder.setPositiveButton("Close", (dialog, which) -> dialog.dismiss());
+            builder.show();
+        });
     }
 
     @Override
     public int getItemCount() {
-        return dataSet.size();
+        return filteredList.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                List<DataModel> filteredResults = new ArrayList<>();
+
+                if (constraint == null || constraint.length() == 0) {
+                    filteredResults.addAll(dataSet);
+                } else {
+                    String filterPattern = constraint.toString().toLowerCase().trim();
+
+                    for (DataModel item : dataSet) {
+                        if (item.getName().toLowerCase().contains(filterPattern)) {
+                            filteredResults.add(item);
+                        }
+                    }
+                }
+
+                FilterResults results = new FilterResults();
+                results.values = filteredResults;
+                results.count = filteredResults.size();
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                filteredList.clear();
+                filteredList.addAll((List<DataModel>) results.values);
+                notifyDataSetChanged();
+            }
+        };
     }
 }
--- app/src/main/java/com/example/ex01_recycleview/fragments/oneFragment.java
+++ app/src/main/java/com/example/ex01_recycleview/fragments/oneFragment.java
@@ -11,6 +11,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.appcompat.widget.SearchView; // Correct import
+
 import com.example.ex01_recycleview.activitys.CustomeAdapter;
 import com.example.ex01_recycleview.activitys.DataModel;
 import com.example.ex01_recycleview.activitys.MyData;
@@ -18,69 +20,28 @@ import com.example.ex01_recycleview.R;
 
 import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link oneFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class oneFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     private ArrayList<DataModel> dataset;
     private RecyclerView recyclerView;
-    private LinearLayoutManager layoutManager;
     private CustomeAdapter adapter;
+    private SearchView searchView;
+
     public oneFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment oneFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static oneFragment newInstance(String param1, String param2) {
-        oneFragment fragment = new oneFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_one, container, false);
 
         recyclerView = view.findViewById(R.id.recycleViewResult);
-        layoutManager = new LinearLayoutManager(getContext());
-        recyclerView.setLayoutManager(layoutManager);
+        searchView = view.findViewById(R.id.searchView); // No casting needed
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
 
-        dataset = new ArrayList<DataModel>();
-
+        dataset = new ArrayList<>();
 
         for (int i = 0; i < MyData.nameArray.length; i++) {
             dataset.add(new DataModel(
@@ -91,9 +52,21 @@ public class oneFragment extends Fragment {
             ));
         }
 
-        adapter = new CustomeAdapter(dataset);
+        adapter = new CustomeAdapter(dataset, getContext());
         recyclerView.setAdapter(adapter);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                adapter.getFilter().filter(query);
+                return false;
+            }
 
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
 
         return view;
     }
